b33038574d767ac8600d14e8b6074b3d128d4728
Samy Dindane <samy@dindane.com>
2020-06-09 12:47:43 +0200

    Add Ctrl to Kyria's thumb keys + Add Space on the left hand

--- keyboards/kyria/keymaps/dinduks/keymap.c
+++ keyboards/kyria/keymaps/dinduks/keymap.c
@@ -73,17 +73,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |Ctrl/ESC|   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | Space|      |  |      |      |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Alt  | GUI  | Enter|LShift|      |  |      |LShift| Space| Bksp | AltGr|
+ *                        | Alt  | GUI  | Enter|LShift| Ctrl |  | Ctrl |LShift| Space| Bksp | AltGr|
  *                        |      |      | Lower|      |      |  |      |      | Raise| Symb |      |
  *                        `----------------------------------'  `----------------------------------'
  */
   [_QWERTY] = LAYOUT(
     KC_TAB,                  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BSLS_GRV),
     MT(MOD_LCTL,KC_ESC),     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                     KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN_COLN), KC_QUOT,
-    KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______, _______, _______, _______, KC_N,    KC_M,    TD(TD_COMM_CCEDILLE), TD(TD_DOT_ELLIPSIS),  KC_SLSH, MT(MOD_RSFT,KC_CAPS),
-             KC_LALT, KC_LGUI, LT(_LOWER,KC_ENT), KC_LSFT, _______, _______, KC_LSFT, LT(_RAISE,KC_SPC), LT(_SYMBOLS, KC_BSPC), KC_RALT
+    KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_SPC,  _______, _______, _______, KC_N,    KC_M,    TD(TD_COMM_CCEDILLE), KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_CAPS),
+             KC_LALT, KC_LGUI, LT(_LOWER,KC_ENT), KC_LSFT, KC_LCTL, KC_LCTL, KC_LSFT, LT(_RAISE,KC_SPC), KC_BSPC, KC_RALT
   ),
 
 /*
